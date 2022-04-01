37ffb5aa30142606ba94693f2f5362b597c1f9bc
szymoninetum <szymon.laskowicz@inetum.com>
2022-04-01 09:20:46 +0200

    environmental variables added

--- app/database.py
+++ app/database.py
@@ -1,8 +1,21 @@
+import os
+from dotenv import load_dotenv
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
+load_dotenv(dotenv_path=".env")
+
+
+POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+POSTGRES_DB: str = os.getenv("POSTGRES_DB", "fastapi_database")
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+#DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
+
 
 engine = _sql.create_engine(DATABASE_URL)
 
