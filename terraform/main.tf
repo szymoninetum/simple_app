ced143312d5d8bceca3455d95a9ce6051e7e2a45
szymoninetum <szymon.laskowicz@inetum.com>
2022-06-02 22:04:11 +0200

    Public IPv4 on EC2 working properly

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,15 +12,17 @@ provider "aws" {
   region = var.aws_region
 }
 
+#Network_interface
 resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.private-subnet-1.id
-  private_ips = ["10.0.2.100"]
+  subnet_id   = aws_subnet.public-subnet-1.id
+  private_ips = ["10.0.0.100"]
 
   tags = {
     Name = "primary_network_interface"
   }
 }
 
+#EC2 instance
 resource "aws_instance" "app_server" {
   ami           = "ami-0022f774911c1d690"    
   instance_type = var.instance_type
@@ -37,7 +39,7 @@ resource "aws_instance" "app_server" {
 }
 
 
-
+#Configure Security Groups
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.vpc.id
 
@@ -61,6 +63,98 @@ resource "aws_security_group" "main" {
     Name = "sg_main"
   }
 }
+
+#ssh sg configure
+resource "aws_security_group" "ssh-security-group" {
+  name        = "SSH Security Group"
+  description = "Enable SSH access on Port 22"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "SSH Security Group"
+  }
+}
+
+# Create Security Group for the Web Server
+# Create Security Group for the Application Load Balancer
+# terraform aws create security group
+resource "aws_security_group" "alb-security-group" {
+  name        = "ALB Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "ALB Security Group"
+  }
+}
+
+# Create Security Group for the Web Server
+# terraform aws create security group
+resource "aws_security_group" "webserver-security-group" {
+  name        = "Web Server Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443 via ALB and SSH access on Port 22 via SSH SG"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = ["${aws_security_group.alb-security-group.id}"]
+  }
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups  = ["${aws_security_group.ssh-security-group.id}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "Web Server Security Group"
+  }
+}
+
+ # End of the Security Groups
+
 
 ############################
 #VPC
